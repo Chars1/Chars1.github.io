7e7e1c0846ad00814a85cd7bc6a22f21efa5c95e
Chars1 <44673945+Chars1@users.noreply.github.com>
2021-03-12 20:03:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
 gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
 // IMG
-gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 100})
 
 // INFORMATION
 gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
